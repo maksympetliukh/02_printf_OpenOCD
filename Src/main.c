c8d4cdf48f50af36b0faef939277b4930145b127
Maksym Petliukh <maksympetliukh@icloud.com>
2026-01-15 18:35:44 +0100

    project implemented

--- Src/main.c
+++ Src/main.c
@@ -17,13 +17,17 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-int main(void)
-{
-    /* Loop forever */
+extern void initialise_monitor_handles(void);
+
+int main(void){
+	initialise_monitor_handles();
+
+	printf("Hello World\n");
 	for(;;);
 }
